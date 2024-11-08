a99b09e4b32bc09aaee298c94a79e0e500476e9a
UDPN-zhangXueFeng <84691916+UDPN-zhangXueFeng@users.noreply.github.com>
2024-11-08 16:17:38 +0800

    update

--- src/views/task/create.vue
+++ src/views/task/create.vue
@@ -184,7 +184,8 @@ const submitForm = async () => {
         if (response.code === 1) {
           ElMessage.success('任务下发成功');
           // router.push(`/task-detail/${response.data.task_id}`); // 提交成功后跳转到首页
-          router.push(`/qc-create/${response.data.task_id}/${route.params.id}`);
+          // router.push(`/qc-create/${response.data.task_id}/${route.params.id}`);
+          router.push(`/task-list`);
 
         } else {
           // ElMessage.error(response.msg || '任务下发失败');
