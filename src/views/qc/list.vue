f5f52f5620ec7328b5c9bf11dbe22904650f27fa
UDPN-zhangXueFeng <84691916+UDPN-zhangXueFeng@users.noreply.github.com>
2024-11-12 16:22:22 +0800

    update

--- src/views/qc/list.vue
+++ src/views/qc/list.vue
@@ -72,11 +72,9 @@ const searchFields = computed(() => [
 ]);
 
 const tableColumns = [
-  { prop: "task_number", label: "质控通知单号", width: "250" },
+  { prop: "qc_number", label: "质控通知单号", width: "250" },
   { prop: "order_number", label: "关联委托单号", width: "250" },
-  { prop: "qc_number", label: "关联任务单号", width: "250", formatter: (row: any) => {
-    return row.order_number === null ? "--" : row.order_number.split('-')[0]
-  } },
+  { prop: "task_number", label: "关联任务单号", width: "250" },
   { prop: "task_related_office", label: "有关科室", width: "120" },
   { prop: "status_text", label: "状态", slot: "status", width: "100" },
   { prop: "createdby", label: "制单人", width: "120" },
